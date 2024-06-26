dd2161b7128130f76e5699cb9bd28ad0608d3ee3
Ayush Bobale <bobaleayush@gmail.com>
2024-06-26 22:20:29 +0530

    File tranfer basic + tailwind
    file transfer pregoress to be implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,42 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 // import { useTunnelIO } from "./hooks/useTunnelIO";
 import { useTunnelIO } from "@ayushbobale/tunnel-react";
 
 function App() {
   const [isInitiator, setIsInitiator] = useState(false);
+  const [files, setFiles] = useState<FileList | null>(null);
+  const localVideoRef = useRef<HTMLVideoElement>(null);
+  const remoteVideoRef = useRef<HTMLVideoElement>(null);
   // const [tunnel, setTunnel] = useState<TunnelIO | null>(null);
   const tunnelIO = useTunnelIO({
-    tunnelArgs: { isInitiator },
+    tunnelArgs: {
+      isInitiator,
+      videoRefs: { local: localVideoRef, remote: remoteVideoRef },
+    },
   });
 
   // console.log(tunnel);
   const [peer, setPeer] = useState("");
   const [msg, setMsg] = useState("");
   return (
-    <div className="p-4">
+    <div className="p-4 text-sm">
       <div className="flex flex-col gap-2 mb-2">
-        <a href="https://github.com/AyushBobale/tunnel-tester" target="_blank">
+        <a
+          className="underline"
+          href="https://github.com/AyushBobale/tunnel-tester"
+          target="_blank"
+        >
           Visit Project
         </a>
         <a
+          className="underline"
           href="https://www.npmjs.com/package/@ayushbobale/tunnel-io"
           target="_blank"
         >
           @ayushbobale/tunnel-io
         </a>
         <a
+          className="underline"
           href="https://www.npmjs.com/package/@ayushbobale/tunnel-react"
           target="_blank"
         >
@@ -32,8 +44,11 @@ function App() {
         </a>
       </div>
 
-      <label htmlFor="isInitiator">Is Initiator</label>
+      <label htmlFor="isInitiator" className="mr-2">
+        Is Initiator
+      </label>
       <input
+        className="px-2 py-1 border"
         id="isInitiator"
         type="checkbox"
         checked={isInitiator}
@@ -44,12 +59,14 @@ function App() {
       />
       <br />
       <input
+        className="px-2 py-1 border"
         type="text"
         value={peer}
         onChange={(e) => setPeer(e.target.value)}
         placeholder="peer"
       />
       <button
+        className="px-2 py-1 border"
         onClick={() => {
           tunnelIO.setPeer(JSON.parse(peer));
         }}
@@ -57,6 +74,7 @@ function App() {
         Set Peer
       </button>
       <button
+        className="px-2 py-1 border"
         onClick={() => {
           navigator.clipboard.writeText(
             JSON.stringify(tunnelIO.tunnelState.localDescription)
@@ -67,12 +85,14 @@ function App() {
       </button>
       <br />
       <input
+        className="px-2 py-1 border"
         type="text"
         value={msg}
         onChange={(e) => setMsg(e.target.value)}
         placeholder="msg"
       />
       <button
+        className="px-2 py-1 border"
         onClick={() => {
           tunnelIO.sendMessage(msg);
         }}
@@ -80,7 +100,50 @@ function App() {
         Send Msg
       </button>
       <br />
-      <pre>{JSON.stringify(tunnelIO.tunnelState, null, "\t")}</pre>
+      <label htmlFor="file-input">
+        <p>Select File</p>
+        <input
+          id="file-input"
+          type="file"
+          multiple
+          onChange={(e) => setFiles(e.target.files)}
+        />
+      </label>
+      <button
+        className="px-2 py-1 border"
+        onClick={() => {
+          console.log({ files });
+          if (files) {
+            tunnelIO.sendFiles(files);
+          }
+        }}
+      >
+        Send File
+      </button>
+      <br />
+      {/* <button
+        className="px-2 py-1 border"
+        onClick={() => {
+          tunnelIO.setMediaDevicesVideo();
+        }}
+      >
+        Set up video
+      </button>
+      <button
+        className="px-2 py-1 border"
+        onClick={() => {
+          tunnelIO.setMediaDevicesVideo();
+        }}
+      >
+        Stop video
+      </button>
+      <div className="flex gap-2">
+        <video className="border" ref={localVideoRef} autoPlay muted />
+        <video className="border" ref={remoteVideoRef} autoPlay />
+      </div> */}
+      <pre className="px-2 py-1 border">
+        {JSON.stringify(tunnelIO.tunnelState, null, "\t")}
+      </pre>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import commonjs from "vite-plugin-commonjs";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), commonjs()],
-  server: {
-    host: "192.168.1.5",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
+  // server: {
+  //   host: "192.168.1.5",
+  // },
 });
